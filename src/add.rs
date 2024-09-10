b3799fe81d65c83be197fbeeff0e3278f3b568c8
VivvMich <mvicorp@hotmail.fr>
2024-09-10 10:29:04 +0200

    test de serde pour ecrire un hmap dans un fichier

--- src/add.rs
+++ src/add.rs
@@ -1,7 +1,9 @@
 use std::collections::HashMap;
-
-pub fn add(name: String, skill_value: u8, mut list : &mut HashMap<String, u8>) {
-    let n = name.clone();
-    list.insert(name, skill_value);
-    print!("Ajout de {} avec une compétence de {}/20.", n, skill_value);
-}
+use std::fs::File;
+use serde_json;
+use crate::Commands::Add;
+// pub fn add(name: String, skill_value: u8, mut list : &mut HashMap<String, u8>) {
+//     let n = name.clone();
+//     list.insert(name, skill_value);
+//     print!("Ajout de {} avec une compétence de {}/20.", n, skill_value);
+// }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,23 @@
 mod add;
+mod shuffle;
+mod show;
 
 use std::collections::HashMap;
 use clap::{Parser, Subcommand};
+use serde_derive::{Deserialize, Serialize};
+use serde_json::{to_writer};
+use std::fs::File;
 
 #[derive(Parser)]
 struct Cli {
     #[clap(subcommand)]
     subcommand: Commands,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Trainee {
+    name: String,
+    skill_value: u8
 }
 
 #[derive(clap::Subcommand)]
@@ -15,15 +26,37 @@ enum Commands{
         name: String,
         skill_value: u8
     },
-    Shuffle{
-    }
+    Shuffle{},
+    Purge {}
 }
 
 fn main() {
     let args = Cli::parse();
-    let mut list: HashMap<String, u8> = HashMap::new();
+
+    // Open file
+    let mut f = File::options()
+        .read(true)
+        .write(true)
+        .open("data.json")
+        .unwrap();
+
+    // pour revenir ou debut du fichier
+    //let _ = f.seek(std::io::SeekFrom::Start(0)).unwrap();
+
+    // pour ecrire avec serde dans le fichier
+    //serde_json::to_writer(f, &q).unwrap();
+
+    let mut list: HashMap<&str, u8> = HashMap::new();
+
+    list.insert("Coucou", 5);
+    list.insert("Baba", 50);
+    list.insert("Zie", 74);
+
+
+
     match args.subcommand {
-        Commands::Add {name, skill_value } => add::add(name, skill_value, &mut list),
-        Commands::Shuffle {} => print!("Shuffle !")
+        Commands::Add {name, skill_value } => print!("Add !"),
+        Commands::Shuffle {} => print!("Shuffle !"),
+        Commands::Purge {} => print!("Purge !")
     }
 }
