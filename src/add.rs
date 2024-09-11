4f2bf5d8e10f42603a8b3a60d17e88ee3fcfb475
Korb VanBonobo <mvicorp@hotmail.fr>
2024-09-11 23:36:30 +0200

    "serialise fake data in json file"

--- src/add.rs
+++ src/add.rs
@@ -1,7 +1,7 @@
 use std::collections::HashMap;
 use std::fs::File;
-use serde_json;
-use crate::Commands::Add;
+//use serde_json;
+//use crate::Commands::Add;
 // pub fn add(name: String, skill_value: u8, mut list : &mut HashMap<String, u8>) {
 //     let n = name.clone();
 //     list.insert(name, skill_value);
--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,45 @@
-mod add;
-mod shuffle;
-mod show;
+//mod add;
+//mod shuffle;
+//mod show;
 
 use std::collections::HashMap;
 use clap::{Parser, Subcommand};
 use serde_derive::{Deserialize, Serialize};
 use serde_json::{to_writer};
 use std::fs::File;
+use std::io::Write;
 
-#[derive(Parser)]
-struct Cli {
-    #[clap(subcommand)]
-    subcommand: Commands,
-}
+// #[derive(Parser)]
+// struct Cli {
+//     #[clap(subcommand)]
+//     subcommand: Commands,
+// }
+//
+// #[derive(Serialize, Deserialize, Debug)]
+// struct Trainee {
+//     name: String,
+//     skill_value: u8
+// }
+//
+// #[derive(clap::Subcommand)]
+// enum Commands{
+//     Add{
+//         name: String,
+//         skill_value: u8
+//     },
+//     Shuffle{},
+//     Purge {}
+// }
 
-#[derive(Serialize, Deserialize, Debug)]
-struct Trainee {
-    name: String,
-    skill_value: u8
-}
-
-#[derive(clap::Subcommand)]
-enum Commands{
-    Add{
-        name: String,
-        skill_value: u8
-    },
-    Shuffle{},
-    Purge {}
-}
-
-fn main() {
-    let args = Cli::parse();
+fn main() -> std::io::Result<()> {
+    // let args = Cli::parse();
 
     // Open file
-    let mut f = File::options()
-        .read(true)
-        .write(true)
-        .open("data.json")
-        .unwrap();
+    // let mut f = File::options()
+    //     .read(true)
+    //     .write(true)
+    //     .open("data.json")
+    //     .unwrap();
 
     // pour revenir ou debut du fichier
     //let _ = f.seek(std::io::SeekFrom::Start(0)).unwrap();
@@ -52,11 +53,17 @@ fn main() {
     list.insert("Baba", 50);
     list.insert("Zie", 74);
 
+    let serial = serde_json::to_string(&list).unwrap();
+    let mut file = File::create("data.json").unwrap();
+    file.write_all(serial.as_bytes())?;
 
+    println!("{}", serial);
 
-    match args.subcommand {
-        Commands::Add {name, skill_value } => print!("Add !"),
-        Commands::Shuffle {} => print!("Shuffle !"),
-        Commands::Purge {} => print!("Purge !")
-    }
+    Ok(())
+
+    // match args.subcommand {
+    //     Commands::Add {name, skill_value } => print!("Add !"),
+    //     Commands::Shuffle {} => print!("Shuffle !"),
+    //     Commands::Purge {} => print!("Purge !")
+    // }
 }
