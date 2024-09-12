562c49cc3867881c978438c06949949f4fbcc1b7
VivvMich <mvicorp@hotmail.fr>
2024-09-12 09:26:20 +0200

    add Add option

--- src/add.rs
+++ src/add.rs
@@ -1,9 +1,21 @@
 use std::collections::HashMap;
 use std::fs::File;
-//use serde_json;
-//use crate::Commands::Add;
-// pub fn add(name: String, skill_value: u8, mut list : &mut HashMap<String, u8>) {
-//     let n = name.clone();
-//     list.insert(name, skill_value);
-//     print!("Ajout de {} avec une compétence de {}/20.", n, skill_value);
-// }
+use serde_json;
+use std::io::Write;
+use crate::Commands::Add;
+
+pub fn add(name: String, skill_value: u8, mut list : &mut HashMap<String, u8>) -> std::io::Result<()> {
+
+    let n = name.clone();
+    list.insert(name, skill_value);
+
+    let serial = serde_json::to_string(&list).unwrap();
+    let mut file = File::create("data.json").unwrap();
+    file.write_all(serial.as_bytes())?;
+
+    println!("{}", serial);
+
+    print!("Ajout de {} avec une compétence de {}/20.", n, skill_value);
+
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,18 @@
-//mod add;
+mod add;
 //mod shuffle;
 //mod show;
+
 
 use std::collections::HashMap;
 use clap::{Parser, Subcommand};
 use serde_derive::{Deserialize, Serialize};
-use serde_json::{to_writer};
-use std::fs::File;
-use std::io::Write;
 
-// #[derive(Parser)]
-// struct Cli {
-//     #[clap(subcommand)]
-//     subcommand: Commands,
-// }
+#[derive(Parser)]
+struct Cli {
+    #[clap(subcommand)]
+    subcommand: Commands,
+}
+
 //
 // #[derive(Serialize, Deserialize, Debug)]
 // struct Trainee {
@@ -21,49 +20,27 @@ use std::io::Write;
 //     skill_value: u8
 // }
 //
-// #[derive(clap::Subcommand)]
-// enum Commands{
-//     Add{
-//         name: String,
-//         skill_value: u8
-//     },
-//     Shuffle{},
-//     Purge {}
-// }
+
+#[derive(clap::Subcommand)]
+enum Commands{
+    Add{
+        name: String,
+        skill_value: u8
+    },
+    // Shuffle{},
+    // Purge {}
+}
 
 fn main() -> std::io::Result<()> {
-    // let args = Cli::parse();
+    let args = Cli::parse();
 
-    // Open file
-    // let mut f = File::options()
-    //     .read(true)
-    //     .write(true)
-    //     .open("data.json")
-    //     .unwrap();
+    let mut list: HashMap<String, u8> = HashMap::new();
 
-    // pour revenir ou debut du fichier
-    //let _ = f.seek(std::io::SeekFrom::Start(0)).unwrap();
-
-    // pour ecrire avec serde dans le fichier
-    //serde_json::to_writer(f, &q).unwrap();
-
-    let mut list: HashMap<&str, u8> = HashMap::new();
-
-    list.insert("Coucou", 5);
-    list.insert("Baba", 50);
-    list.insert("Zie", 74);
-
-    let serial = serde_json::to_string(&list).unwrap();
-    let mut file = File::create("data.json").unwrap();
-    file.write_all(serial.as_bytes())?;
-
-    println!("{}", serial);
-
-    Ok(())
-
-    // match args.subcommand {
-    //     Commands::Add {name, skill_value } => print!("Add !"),
+    match args.subcommand {
+        Commands::Add {name, skill_value } => add::add(name, skill_value, &mut list)?,
     //     Commands::Shuffle {} => print!("Shuffle !"),
     //     Commands::Purge {} => print!("Purge !")
-    // }
+    }
+
+    Ok(())
 }
